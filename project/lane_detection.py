53ac4800fbf802369643253e0ba798291e6b166a
Philipp Gehrig <philipp@gehrig.info>
2024-03-27 19:44:04 +0100

    wip: setup

--- project/lane_detection.py
+++ project/lane_detection.py
@@ -1,10 +1,24 @@
 from __future__ import annotations
-
+import numpy as np
 
 class LaneDetection:
 
     def __init__(self):
+        
         pass
 
     def detect(self):
         pass
+
+    def makeGray(self):
+        pass
+
+    def applyFilters(self):
+        pass
+    
+    def detectBorders(self):
+        pass
+
+    def detectLanes(self):
+        pass
+    
