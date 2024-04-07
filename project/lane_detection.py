ffc54a8c733761eebe66bff989dc8a467e3a8198
Jona Bergmann <inf22037@lehre.dhbw-stuttgart.de>
2024-04-07 10:13:26 +0200

    feat(lane_detection): Initial attempt for finding a function for lateral control -> Not working yet!

--- project/lane_detection.py
+++ project/lane_detection.py
@@ -27,6 +27,7 @@ class LaneDetection:
         self.convolution()
         self.relu()
         self.debug_image = self.img
+        print(self.img)
 
     def convolution(self):
         if not self.isGrayScale:
@@ -55,7 +56,7 @@ class LaneDetection:
         for x in range(self.img.shape[0]):
             for y in range(self.img.shape[1]):
                 p = getPixel(self.img,x,y)
-                self.img[y][x] = p if p<105 else 255
+                self.img[y][x] = 0 if p<105 else 255
 
     def smooth(self):
         if self.isGrayScale:
@@ -96,3 +97,43 @@ def multiplyMatrix(img: Image, pos: tuple[int, int], matrix) -> tuple[int]:
 
 def getPixel(arr, x, y):
     return arr[y][x]
+
+def find_car_indices(self):
+    # start in the middle of the array to look for the car
+    mid_x_idx = self.shape[1] // 2
+    mid_y_idx = self.shape[0] // 2
+
+    car_idx = None
+    mid_y_below = np.arange(mid_y_idx, self.shape[0])               # create an array with all lines of self below the middle
+    car_y_idx = np.where(self[mid_y_below, mid_x_idx] == 0)[0]      # find the indices of the black pixels
+    if car_y_idx.size > 0:
+        car_idx = mid_y_idx + car_y_idx[0]
+
+    return car_idx         # is the position of the car always the same?
+
+def find_line_indices(self):
+    # start in the middle of the array to look for both lines
+    mid_x_idx = self.shape[1] // 2
+    mid_y_idx = self.shape[0] // 2
+
+    # search for the first black pixel on the left
+    left_x_idx = mid_x_idx
+    left_row = self[mid_y_idx, :mid_x_idx] == 0                 # converting the left half of the row to boolean: 0 -> True
+    if np.any(left_row):                                        # np.any() checks if row has any black pixels
+        left_x_idx = mid_x_idx - np.argmax(left_row[::-1])      # np.argmax() looks for the first True starting in the middle
+
+    right_x_idx = mid_x_idx
+    right_row = self[mid_y_idx, mid_x_idx:] == 0
+    if np.any(right_row):
+        right_x_idx = mid_x_idx + np.argmax(right_row)
+
+    return left_x_idx, right_x_idx
+
+def linespace(self, left, right):
+    if left is not None and right is not None:
+        mid = (left + right) // 2
+        mid_val = 0
+        left_val = -1
+        right_val = 1
+        interp_val = np.linspace(left_val, right_val, right - left + 1)
+        self[mid, left:right + 1] = np.around(np.interp(np.arange(left, right + 1),[left, right], [left_val, right_val]))
