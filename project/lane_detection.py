04e2ed2e92cb79192dd5ba4729a6b7845735ac5a
Philipp Gehrig <philipp@gehrig.info>
2024-04-15 16:55:03 +0200

    fix[regression]: moved regression to path planing

--- project/lane_detection.py
+++ project/lane_detection.py
@@ -23,9 +23,8 @@ class LaneDetection:
         self.toGrayScale()
         self.convolution()
         self.relu()
-        print(np.shape(self.img))       # shape of state_img: (96, 80)
         self.regression()
-        self.debug_image = self.img
+        self.debug_image = self.img 
 
     def toGrayScale(self):
         coefficients = np.array([0.2126, 0.7152, 0.0722])
@@ -51,18 +50,4 @@ class LaneDetection:
         threshold = 105
         self.img = np.where(self.img < threshold, 0, 255)
     
-    def regression(self):
-        # Fit a line to the bottom half of the image
-        y = np.arange(40, 80)
-        x = np.zeros(40)
-        for i in range(40):
-            x[i] = np.argmax(self.img[40 + i, :])
-        coefficients = np.polyfit(y, x, 4)
-        self.poly_func = np.poly1d(coefficients)
-        self.img = np.zeros((80, 80))
-        for i in range(80):
-            # Clip the predicted index to ensure it is within the valid range
-            predicted_index = int(self.poly_func(i))
-            clipped_index = np.clip(predicted_index, 0, self.img.shape[1] - 1)
-            self.img[i, clipped_index] = 255
-            
+  
--- project/path_planning.py
+++ project/path_planning.py
@@ -1,10 +1,28 @@
 from __future__ import annotations
-
+import numpy as np
 
 class PathPlanning:
 
     def __init__(self):
+        self.debug_image = None
         pass
 
-    def plan(self):
+    def plan():
+        regression()
         pass
+
+def regression(self):
+    # Fit a line to the bottom half of the image
+    y = np.arange(40, 80)
+    x = np.zeros(40)
+    for i in range(40):
+        x[i] = np.argmax(self.img[40 + i, :])
+    coefficients = np.polyfit(y, x, 2)
+    self.poly_func = np.poly1d(coefficients)
+    self.img = np.zeros((80, 80))
+    for i in range(80):
+        # Clip the predicted index to ensure it is within the valid range
+        predicted_index = int(self.poly_func(i))
+        clipped_index = np.clip(predicted_index, 0, self.img.shape[1] - 1)
+        self.img[i, clipped_index] = 255
+        
