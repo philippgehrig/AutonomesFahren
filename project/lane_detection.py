da1d11700668dbd4bd7245e8b3872da991c35cc3
Jona Bergmann <inf22037@lehre.dhbw-stuttgart.de>
2024-04-16 13:38:38 +0200

    feat(lane_detection): Added build_lanes to decide whether a pixel belongs to the left or the right lane. Can't handle more then four white pixels per row.

--- project/lane_detection.py
+++ project/lane_detection.py
@@ -25,15 +25,21 @@ class LaneDetection:
 
     def detect(self, state_image):
         self.img = np.array(state_image)[0:80, :]
-        #self.remove_car()
         self.toGrayScale()
         self.convolution()
         self.relu()
-        self.remove_car()
-        #self.calculate_path()
-        #self.regression()
+        self.remove_pixel()
         self.debug_image = self.img
         #print(np.shape(self.img))       # original shape of state_img: (96, 96)
+        left_lane, right_lane = self.build_lanes()
+        print("Left Lane:")
+        for point in left_lane:
+            print(point)            # format: (x, y)
+
+        print("Right Lane:")
+        for point in right_lane:
+            print(point)
+
 
     def toGrayScale(self):
         coefficients = np.array([0.2126, 0.7152, 0.0722])
@@ -59,15 +65,71 @@ class LaneDetection:
         threshold = 105
         self.img = np.where(self.img < threshold, 0, 255)
 
-    def remove_car(self):
+    def remove_pixel(self):
         # overwrite the pixels of the car
         for i in range(67, 77):
+            self.img[i][45] = 0
             self.img[i][46] = 0
             self.img[i][47] = 0
             self.img[i][48] = 0
             self.img[i][49] = 0
-        # overwrite the wrong pixels in the last line of the image
-        self.img[79] = 0
+            self.img[i][50] = 0
+        # overwrite the wrong pixels on the border of the image to prevent calculation errors
+        self.img[0, :] = 0
+        self.img[-1, :] = 0
+        self.img[:, 0] = 0
+        self.img[:, -1] = 0
+
+
+    def build_lanes(self):
+        left_lane = []
+        right_lane = []
+
+        centroid = np.mean(np.where(self.img == 255)[1])
+        middle_index = self.img.shape[1] // 2
+
+        for row in range(self.img.shape[0]):
+            white_pixel_indices = np.where(self.img[row] == 255)[0]
+            if len(white_pixel_indices) == 2:
+                left_lane.append((row, white_pixel_indices[0]))
+                right_lane.append((row, white_pixel_indices[1]))
+            elif centroid < middle_index:
+                # left curve
+                if len(white_pixel_indices) == 1:
+                    left_lane.append((row, 'NaN'))
+                    right_lane.append((row, white_pixel_indices[0]))
+                elif len(white_pixel_indices) == 3:
+                    right_lane.append((row, white_pixel_indices[0]))
+                    left_lane.append((row, white_pixel_indices[1]))
+                    right_lane.append((row, white_pixel_indices[2]))
+                elif len(white_pixel_indices) == 4:
+                    right_lane.append((row, white_pixel_indices[0]))
+                    left_lane.append((row, white_pixel_indices[1]))
+                    left_lane.append((row, white_pixel_indices[2]))
+                    right_lane.append((row, white_pixel_indices[3]))
+                else:
+                    print('more then four white pixels')
+            elif centroid > middle_index:
+                # right curve
+                if len(white_pixel_indices) == 1:
+                    right_lane.append((row, 'NaN'))
+                    left_lane.append((row, white_pixel_indices[0]))
+                elif len(white_pixel_indices) == 3:
+                    left_lane.append((row, white_pixel_indices[0]))
+                    right_lane.append((row, white_pixel_indices[1]))
+                    left_lane.append((row, white_pixel_indices[2]))
+                elif len(white_pixel_indices) == 4:
+                    left_lane.append((row, white_pixel_indices[0]))
+                    right_lane.append((row, white_pixel_indices[1]))
+                    right_lane.append((row, white_pixel_indices[2]))
+                    left_lane.append((row, white_pixel_indices[3]))
+                else:
+                    print('more then four white pixels')
+            else:
+                print('no curve detected')
+
+        return left_lane, right_lane
+
 
 
 
